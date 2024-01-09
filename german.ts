04237ea12e5d5c3e4e5eda052b5394cf911fdd5b
Simon-Friedrich Böttger <email@simonboettger.de>
2024-01-09 14:21:28 +0100

    Add german translation

---
+++ german.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+<context>
+    <name>PdfFile</name>
+    <message>
+        <location filename="src/pdffile.cpp" line="290"/>
+        <location filename="src/pdffile.cpp" line="323"/>
+        <location filename="src/pdffile.cpp" line="331"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="src/pdffile.cpp" line="290"/>
+        <source>Error writing output pdf </source>
+        <translation>Fehler beim Schreiben von </translation>
+    </message>
+    <message>
+        <location filename="src/pdffile.cpp" line="323"/>
+        <source>Error opening </source>
+        <translation>Fehler beim Öffnen von </translation>
+    </message>
+    <message>
+        <location filename="src/pdffile.cpp" line="331"/>
+        <source>Error getting first page</source>
+        <translation>Fehler beim Öffnen der ersten Seite</translation>
+    </message>
+    <message>
+        <location filename="src/pdffile.cpp" line="379"/>
+        <source>Invoice</source>
+        <translation>Rechnung</translation>
+    </message>
+</context>
+<context>
+    <name>PdfFileList</name>
+    <message>
+        <location filename="src/pdffile.cpp" line="192"/>
+        <source>Error Deleting file</source>
+        <translation>Fehler beim Löschen der Datei</translation>
+    </message>
+</context>
+<context>
+    <name>cfMain</name>
+    <message>
+        <location filename="src/cfmain.cpp" line="193"/>
+        <source>Directory does not exist</source>
+        <translation>Verzeichnis existiert nicht</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="193"/>
+        <source>This directory does not exist. Do you want to create it?</source>
+        <translation>Dieses Verzeichnis gibt es nicht. Wollen Sie es erstellen?</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="199"/>
+        <source>Error while creating directory</source>
+        <translation>Fehler bei der Verzeichnis Erstellung</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="212"/>
+        <source>Overwrite file?</source>
+        <translation>Datei überschreiben?</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="212"/>
+        <source>This file already exists. Do you really want to overwrite </source>
+        <translation>Datei bereits vorhanden. Wollen Sie </translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="212"/>
+        <source> ?</source>
+        <translation> überschreiben?</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="231"/>
+        <location filename="src/cfmain.cpp" line="234"/>
+        <source>Error while renaming</source>
+        <translation>Fehler beim Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="231"/>
+        <source>This file cannot be renamed.</source>
+        <translation>Die Datei konnte nicht umbenannt werden.</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="244"/>
+        <source>Choose target directory</source>
+        <translation>Zielverzeichnis auswählen</translation>
+    </message>
+</context>
+<context>
+    <name>cfOpen</name>
+    <message>
+        <location filename="src/cfopen.cpp" line="45"/>
+        <source>Open PDF file</source>
+        <translation>PDF Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.cpp" line="45"/>
+        <source>PDF files *.pdf</source>
+        <translation>PDF Dateien *.pdf</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.cpp" line="65"/>
+        <source>Open directory</source>
+        <translation>Verzeichnis öffnen</translation>
+    </message>
+</context>
+<context>
+    <name>fOpen</name>
+    <message>
+        <location filename="src/cfopen.h" line="106"/>
+        <source>Scan2OCR</source>
+        <translation>Scan2OCR</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="107"/>
+        <source>Open &amp;file</source>
+        <translation>&amp;Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="108"/>
+        <source>Open &amp;directory</source>
+        <translation>&amp;Verzeichnis öffnen</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="109"/>
+        <source>F&amp;inished</source>
+        <translation>&amp;Fertig</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="110"/>
+        <source>FTP Server</source>
+        <translation>FTP Server</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="111"/>
+        <source>Username</source>
+        <translation>Benutzername</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="112"/>
+        <source>FTP directory</source>
+        <translation>FTP Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="113"/>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location filename="src/cfopen.h" line="114"/>
+        <source>Port</source>
+        <translation>Port</translation>
+    </message>
+</context>
+<context>
+    <name>mWindow</name>
+    <message>
+        <location filename="src/cfmain.cpp" line="55"/>
+        <source>Scan2OCR</source>
+        <translation>Scan2OCR</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="56"/>
+        <source>pdf file name</source>
+        <translation>pdf Dateiname</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="57"/>
+        <source>&amp;Rename</source>
+        <translation>&amp;Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="58"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Löschen</translation>
+    </message>
+    <message>
+        <location filename="src/cfmain.cpp" line="59"/>
+        <source>Target directory:</source>
+        <translation>Zielverzeichnis:</translation>
+    </message>
+</context>
+</TS>
